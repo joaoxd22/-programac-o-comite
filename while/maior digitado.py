90521d8bf76b8fb6e8491e9894eab6995ccff69c
joaoxd22 <jonixxx807@gmail.com>
2025-09-29 21:52:19 -0300

    continuaçao while

---
+++ while/maior digitado.py
@@ -0,0 +1,10 @@
+numero = 1
+maior_numero = 0
+
+while numero != 0:
+    numero = int(input("informe um numero"))
+
+    if numero > maior_numero:
+        maior_numero = numero
+
+print(f" o maior numero e {maior_numero}") 
